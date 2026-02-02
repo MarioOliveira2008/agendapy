b357dc0bc449a85011f82b9049f53f5cf438856f
MarioOliveira2008 <mario.o.jesus@aluno.senai.br>
2026-02-02 16:48:03 -0300

    feat: continuacao agenda

--- teste.py
+++ teste.py
@@ -1,1 +1,5 @@
-print("Ola, python!")
+from models.database import Database
+
+# Teste do gerenciador de modelos
+with Database('./data/tarefas.sqlite3') as db:
+    db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);',('Usar o gerenciador de contexto', '2026-02-03'))
