62fdb645f4aa54ce1395453d663d3aa3f619df64
MarioOliveira2008 <mario.o.jesus@aluno.senai.br>
2026-02-09 16:15:04 -0300

    feat: atualizando e editando tarefas

--- models/tarefa.py
+++ models/tarefa.py
@@ -53,3 +53,13 @@ class Tarefa:
             resultado: Cursor = db.executar(query, params)
             return resultado
     
+    def atualizar_tarefa(self) -> Cursor:    
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
+
+
+        
+    
